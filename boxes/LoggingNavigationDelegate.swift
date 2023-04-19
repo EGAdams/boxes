26487c5606150df5bdb000800bf4b98d6e9173c7
egadams <eg1972@gmail.com>
2023-04-19 13:36:27 -0400

    the error handler is now catching errors

--- boxes/LoggingNavigationDelegate.swift
+++ boxes/LoggingNavigationDelegate.swift
@@ -1,29 +1,22 @@
-//
-//  LoggingNavigationDelegate.swift
-//
-//  Created by Clay Ackerland on 4/14/23.
-//
-
 import Foundation
 import WebKit
 
-class LoggingNavigationDelegate: NSObject, WKNavigationDelegate, WKScriptMessageHandler { 
+class LoggingNavigationDelegate: NSObject, WKNavigationDelegate {
+    
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print("Navigation request URL: \(navigationAction.request.url?.absoluteString ?? "unknown")") // Add this print statement
+        
         if let url = navigationAction.request.url, url.scheme == "console-log" {
             print("[JavaScript console.log]:", url.absoluteString.removingPercentEncoding?.replacingOccurrences(of: "console-log://", with: "") ?? "")
             decisionHandler(.cancel)
             return
         }
-        decisionHandler( .allow )}
+        decisionHandler(.allow)
+    }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         if let jsFilePath = Bundle.main.path(forResource: "init", ofType: "js"), let jsString = try? String(contentsOfFile: jsFilePath) {
             webView.evaluateJavaScript(jsString, completionHandler: nil)
-        }}
-    
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print( "inside userContentController in LoggingNavigationDelegate... message.name: \(message.name) " )
-        if message.name == "jsLog" {
-            print("JavaScript Log: \(message.body)")
-        }}
+        }
+    }
 }
